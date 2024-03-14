bfeb5907295b201b7b996131e189f205a4c85a25
marlonmarin26 <marlon.1701910281@ucaldas.edu.co>
2024-03-14 17:01:18 -0500

    Open the port of express

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const express = require('express')
-const app = expres();
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const app = express();
 
 // Poder acceder a leer archivo .env
-dotenv.config();
-const PORT = 3005;
+require("dotenv").config();
+const CONNECTION_PORT = process.env.CONNECTION_PORT || 3005;
+
+app.listen(CONNECTION_PORT, () => {
+    console.log(`Using the port ${CONNECTION_PORT}`);
+});
