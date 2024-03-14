2b4a9513142b343adb8d22e7c5c3b5e87a0b53f9
marlonmarin26 <marlon.1701910281@ucaldas.edu.co>
2024-03-14 17:19:36 -0500

    Mongo database connection

--- index.js
+++ index.js
@@ -11,3 +11,19 @@ const CONNECTION_PORT = process.env.CONNECTION_PORT || 3005;
 app.listen(CONNECTION_PORT, () => {
     console.log(`Using the port ${CONNECTION_PORT}`);
 });
+
+const mongo_connect = () => {
+    try {
+        mongoose.connect(process.env.DATABASE_CONNECTION_STRING)
+            .then(() => {
+                console.log('Connected to the database');
+            }).catch((err) => {
+                console.log('Error connecting to the database');
+            });
+
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+mongo_connect();
